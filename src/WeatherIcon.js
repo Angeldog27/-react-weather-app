9816354330b4a9acc1e519b23a0160eca3839a26
Angeldog27 <126614235+Angeldog27@users.noreply.github.com>
2023-06-03 21:18:59 -0400

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -13,7 +13,7 @@ export default function WeatherIcon(props) {
     "04n": "CLOUDY",
     "09d": "RAIN",
     "09n": "RAIN",
-    "010d": "RAIN",
+    "10d": "RAIN",
     "10n": "RAIN",
     "11d": "RAIN",
     "11n": "RAIN",
